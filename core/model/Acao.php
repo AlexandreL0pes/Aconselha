be47ad96fa02c9bbc2bac2e3bbe2d2712e17c5e2
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-03-12 11:04:04 -0300

    Criação da model\Acao

---
+++ core/model/Acao.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace core\model;
+
+use core\CRUD;
+use Exception;
+
+class Acao extends CRUD {
+    const TABELA = "acao";
+    const COL_ID = "id";
+    const COL_NOME = "nome";
+    const COL_DESCRICAO = "descricao";
+
+
+    public function adicionar($dados) {
+        try {
+
+            $retorno = $this->create(self::TABELA, $dados);
+
+        } catch (\Throwable $th) {
+            echo "Mensagem: " . $th->getMessage() . "\n Local: " . $th->getTraceAsString();
+            return false;
+        }
+
+        return $retorno;
+    }
+
+    public function alterar($dados) {
+        if (!isset($dados[self::COL_ID])) {
+            throw new Exception("É necessário informar o ID do perfil");
+        }
+
+        $where_condicao = self::COL_ID . " = ?";
+        $where_valor[] = $dados[self::COL_ID];
+
+        try {
+            $this->update(self::TABELA, $dados, $where_condicao, $where_valor);
+        } catch (\Throwable $th) {
+            echo "Mensagem: " . $th->getMessage() . "\n Local: " . $th->getTraceAsString();
+            return false;
+        }
+
+        return $dados[self::COL_ID];
+
+    }
+
+    public function listar($campos = null, $busca = [], $ordem = null, $limite = null) {
+        
+        $campos = $campos != null ? $campos : " * ";
+        $ordem = $ordem != null ? $ordem : self::COL_ID . " ASC";
+        $limite = $limite != null ? $limite : 10;
+
+        $where_condicao = " 1 = 1 ";
+        $where_valor = [];
+
+        if ($busca && count($busca) > 0){
+            // Fazer a busca por nome 
+            if (isset($busca[self::COL_NOME]) && !empty($busca[self::COL_NOME])){
+                $where_condicao .= " AND " . self::COL_NOME . " LIKE ? ";
+                $where_valor[] = "%{$busca[self::COL_NOME]}%";
+            }
+        }
+
+        $retorno = [];
+
+        try {
+            $retorno = $this->read(null, self::TABELA, $campos, $where_condicao, $where_valor, null, $ordem, $limite);  
+        } catch (\Throwable $th) {
+            echo "Mensagem: " . $th->getMessage() . "\n Local: " . $th->getTraceAsString();
+            return false;
+        }
+
+        return $retorno;
+
+
+    }
+
+}
