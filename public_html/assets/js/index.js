4c8f42b3007ad31ddaabc3a6843e7681e9287ef9
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-04-09 12:10:48 -0300

    Reolução de bug

--- public_html/assets/js/index.js
+++ public_html/assets/js/index.js
@@ -1,6 +1,6 @@
 let events = () => {
-    abrirReuniao();
-
+  abrirReuniao();
+  resposiveNavbar();
 };
 
 /**
@@ -8,16 +8,33 @@ let events = () => {
  * Redireciona o usuário para a página da reunião, correpondente ao card clicado
  */
 let abrirReuniao = () => {
-    const cardsReuniao = document.querySelectorAll('div.cardbox, div.cardbox *');
-    cardsReuniao.forEach(function (card) {
-        card.addEventListener('click', function (event) {
-            let idReuniao = event.target.getAttribute('data-turmaconselho') || event.target.parentElement.getAttribute('data-turmaconselho'); 
-            console.log(idReuniao);
-            window.location = './reuniao.html?idTurmaConselho=' + idReuniao;
-        });
-
+  const cardsReuniao = document.querySelectorAll("div.cardbox, div.cardbox *");
+  cardsReuniao.forEach(function (card) {
+    card.addEventListener("click", function (event) {
+      let idReuniao =
+        event.target.getAttribute("data-turmaconselho") ||
+        event.target.parentElement.getAttribute("data-turmaconselho");
+      console.log(idReuniao);
+      window.location = "./reuniao.html?idTurmaConselho=" + idReuniao;
     });
+  });
 };
 
+let resposiveNavbar = () => {
+  const menuIconEl = document.querySelector(".menu-icon");
+  const sidenavEl = document.querySelector(".sidenav-mobile");
+  const sidenavCloseEl = document.querySelector(".sidenav__close-icon-mobile");
 
+  function toggleClassName(el, className) {
+    el.classList.toggle(className);
+  }
+
+  menuIconEl.addEventListener("click", function () {
+    toggleClassName(sidenavEl, "active");
+  });
+
+  sidenavCloseEl.addEventListener("click", function () {
+    toggleClassName(sidenavEl, "active");
+  });
+};
 events();
