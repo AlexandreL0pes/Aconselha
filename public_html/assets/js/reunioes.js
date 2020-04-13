cd2f6ad7b744c193c11d76779fbf70d95635d5c2
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-04-13 19:07:46 -0300

    Evento de início de conselho

--- public_html/assets/js/reunioes.js
+++ public_html/assets/js/reunioes.js
@@ -1,6 +1,7 @@
 let eventos = () => {
   abrirReuniao();
   selecionarTurmas();
+  iniciarConselhos();
 };
 
 /**
@@ -27,23 +28,21 @@ let abrirReuniao = () => {
  * Seleciona as turmas para iniciar o conselho
  */
 let selecionarTurmas = () => {
-    const cardsTurmas = document.querySelectorAll(
-        ".turmas>div.cardbox"
-    );
-    cardsTurmas.forEach(function (card) {
-        card.addEventListener('click', function (event) {
-            let item = null;
-            
-            if (event.currentTarget.classList.contains('cardbox')) {
-                item = event.currentTarget;                
-            }
-            if (event.currentTarget.parentNode.classList.contains('cardbox')) {
-                item = event.currentTarget.parentNode;
-            }                        
-            item.classList.toggle('selected');
-            habilitarBotao();
-        });
+  const cardsTurmas = document.querySelectorAll(".turmas>div.cardbox");
+  cardsTurmas.forEach(function (card) {
+    card.addEventListener("click", function (event) {
+      let item = null;
+
+      if (event.currentTarget.classList.contains("cardbox")) {
+        item = event.currentTarget;
+      }
+      if (event.currentTarget.parentNode.classList.contains("cardbox")) {
+        item = event.currentTarget.parentNode;
+      }
+      item.classList.toggle("selected");
+      habilitarBotao();
     });
+  });
 };
 
 /**
@@ -51,18 +50,46 @@ let selecionarTurmas = () => {
  * Habilitar botão quando existem turmas selecionadas
  */
 let habilitarBotao = () => {
-    const botao = document.querySelector('#iniciarConselho');
-    const turmasSelecionadas = document.querySelectorAll('.turmas>div.cardbox.selected');
-    const qtdTurmas = document.querySelector('#qtdTurmas');
-    
-    if (turmasSelecionadas.length > 0) {
-        botao.disabled = false;
-        qtdTurmas.innerHTML = turmasSelecionadas.length;
-    }else{
-        botao.disabled = true;
-        qtdTurmas.innerHTML = 'Nenhuma';
-    }
+  const botao = document.querySelector("#iniciarConselho");
+  const turmasSelecionadas = document.querySelectorAll(
+    ".turmas>div.cardbox.selected"
+  );
+  const qtdTurmas = document.querySelector("#qtdTurmas");
+
+  if (turmasSelecionadas.length > 0) {
+    botao.disabled = false;
+    qtdTurmas.innerHTML = turmasSelecionadas.length;
+  } else {
+    botao.disabled = true;
+    qtdTurmas.innerHTML = "Nenhuma";
+  }
 };
 
+let iniciarConselhos = () => {
+  const botao = document.querySelector("#iniciarConselho");
+  botao.addEventListener("click", function (event) {
+    event.preventDefault();
+    const turmasSelecionadas = document.querySelectorAll(
+      ".turmas>div.cardbox.selected"
+    );
+
+    let codigoTurmas = [];
+    for (let i of turmasSelecionadas) {
+      // codigoTurmas.append(i.getAttribute('data-cod-turma'));
+      codigoTurmas.push(i.getAttribute("data-cod-turma"));
+    }
+
+    // const xhr = new XMLHttpRequest();
+
+    // xhr.onload = function () {
+    //   console.log(this.responseText);
+    // };
+
+    // xhr.open("POST", "dom.php");
+    // xhr.setRequestHeader("Content-type", "aplication/x-www-form-urlencoded");
+    // data = JSON.stringify({ firtsName: "Alexandre", lastName: "Lopes" });
+    // xhr.send(data);
+  });
+};
 
 eventos();
