034bda30738c6c99d9890c38604a5cccc07b7d08
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-04-22 18:20:12 -0300

    Implementação de evento ao clicar no encaminhamento

---
+++ public_html/assets/js/atendimentos.js
@@ -0,0 +1,34 @@
+const cardEncaminhamentosListener = () => {
+  const encaminhamentos = document.querySelectorAll(".card-encaminhamento");
+
+  encaminhamentos.forEach((card) => {
+    card.addEventListener(
+      "click",
+      (event) => {
+        abrirEncaminhamento(event);
+      },
+      false
+    );
+  });
+};
+
+const abrirEncaminhamento = (element) => {
+  let encaminhamento = element.currentTarget.getAttribute(
+    "data-encaminhamento"
+  );
+
+  if (encaminhamento) {
+    localStorage.setItem("encaminhamento", encaminhamento);
+    window.location =
+      "./salvarAtendimento.html?encaminhamento=" + encaminhamento;
+  } else {
+    showMessage(
+      "Houve um erro!",
+      "Não foi possível abrir o encaminhamento.",
+      "warning",
+      5000
+    );
+  }
+};
+
+cardEncaminhamentosListener();
