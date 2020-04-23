7bcdb1a15b1dfc7f12c30925185a0d0516403079
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-04-23 20:33:06 -0300

    Redirecionamento das ações do conselho

--- public_html/assets/js/turma.js
+++ public_html/assets/js/turma.js
@@ -1,15 +1,39 @@
 const abrirMemoria = () => {
-    const memoriaLink = document.querySelector('.action-card.memoria, .action-card.memoria >* ');
-    memoriaLink.addEventListener('click', function (params) {
-        
-        if (localStorage.getItem('conselhoAtual')) {
-            window.open('./memoria.html', '_blank');
-        }else{
-            showMessage('Houve um erro!','Selecione um conselho antes de prosseguir.', 'error', 5000);
-            
-        }
-    });
+  const memoriaLink = document.querySelector(
+    ".action-card.memoria, .action-card.memoria >* "
+  );
+  memoriaLink.addEventListener("click", function (params) {
+    if (localStorage.getItem("conselhoAtual")) {
+      window.open("./memoria.html", "_blank");
+    } else {
+      showMessage(
+        "Houve um erro!",
+        "Selecione um conselho antes de prosseguir.",
+        "error",
+        5000
+      );
+    }
+  });
+};
 
+const abrirAtendimentos = () => {
+  const atendimentosCard = document.querySelector(
+    ".action-card.atendimentos, .action-card.atendimentos >* "
+  );
+
+  atendimentosCard.addEventListener("click", (event) => {
+    if (localStorage.getItem("conselhoAtual")) {
+      window.location = "./atendimentos.html";
+    } else {
+      showMessage(
+        "Houve um erro!",
+        "Selecione um conselho antes de prosseguir.",
+        "error",
+        5000
+      );
+    }
+  });
 };
 
 abrirMemoria();
+abrirAtendimentos();
