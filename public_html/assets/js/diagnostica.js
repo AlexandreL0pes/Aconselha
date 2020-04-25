866f501a8d0597380f05cb669999aa85acf42712
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-04-25 12:48:16 -0300

    Adição de novas classes no layout

--- public_html/assets/js/diagnostica.js
+++ public_html/assets/js/diagnostica.js
@@ -84,8 +84,9 @@ const salvarAvaliacao = () => {
     });
   });
 };
+
 /**
- * Esconde o modal, remove o aluno do modal atual e reseta o formulario
+ * Esconde o modal, remove o aluno do modal atual e apaga os perfis selecionados
  * @param {DOM Element} modal Modal de Avaliação Diagnóstica
  */
 const fecharAvaliacao = (modal) => {
@@ -93,6 +94,13 @@ const fecharAvaliacao = (modal) => {
   if (localStorage.getItem("avaliacaoAtual")) {
     localStorage.removeItem("avaliacaoAtual");
   }
+
+  const selectedChips = modal.querySelectorAll(".chip.selected");
+
+  console.log(selectedChips);
+  selectedChips.forEach((chip) => {
+    chip.classList.remove("selected");
+  });
 };
 
 const atualizarAvaliacoesPendentes = () => {
@@ -101,9 +109,46 @@ const atualizarAvaliacoesPendentes = () => {
   const qtdAvaliacoes = document.querySelector("#qtdAvaliacoes");
   qtdAvaliacoes.innerHTML = cards.length - selectedCards.length;
 };
+
+const concluirAvaliacao = () => {
+  const btnSalvar = document.querySelector(".btnSalvar");
+  btnSalvar.addEventListener("click", (event) => {
+    console.log("O botão foi pressionado!");
+
+    // const url = "https://randomuser.me/api/?results="; // Get 10 random users
+    //   fetch(url)
+    //     .then((resp) => {
+    //       console.log(resp.status);
+    //       return resp.json();
+    //     }) // Transform the data into json
+    //     .then(function (data) {
+    //       console.log(data);
+    //     });
+
+    //   const url = 'https://randomuser.me/api';
+    //   // The data we are going to send in our request
+    //   let data = {
+    //       name: 'Sara',
+    //   }
+    //   // Create our request constructor with all the parameters we need
+    //   var request = new Request(url, {
+    //       method: 'POST',
+    //       body: JSON.stringify(data),
+    //       headers: new Headers()
+    //   });
+
+    //   fetch(request)
+    //   .then(function(resp) {
+    //       // Handle response we get from the API
+    //       console.log(resp);
+    //   })
+  });
+};
+
 // fecharAvaliacao();
 atualizarAvaliacoesPendentes();
 openModal();
 closeModal();
 selectPerfil();
 salvarAvaliacao();
+concluirAvaliacao();
