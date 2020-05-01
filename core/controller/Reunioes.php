79b7ce1649bea0eea41fad5c6f6563ed49cea893
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-01 18:22:30 -0300

    Implementação da classe controller\Reunioes

---
+++ core/controller/Reunioes.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace core\controller;
+
+use core\model\Reuniao;
+
+class Reunioes
+{
+
+	private $reuniao_id = null;
+	private $turma_cod = null;
+	private $data = null;
+	private $etapaConselho = null;
+	private $finalizado = null;
+
+
+	public function __set($atributo, $valor)
+	{
+		$this->$atributo = $valor;
+	}
+
+	public function __get($atributo)
+	{
+		return $this->$atributo;
+	}
+
+	public function cadastrar($dados)
+	{
+		$turmas = $dados['turmas'];
+
+		$dataAtual = date('Y-m-d');
+		$etapaAtual = 1;
+
+		$reuniao = new Reuniao();
+
+		foreach ($turmas as $turma) {
+			$resultado = $reuniao->adicionar(
+				[
+					Reuniao::COL_COD_TURMA => $turma,
+					Reuniao::COL_DATA => $dataAtual,
+					Reuniao::COL_ETAPA_CONSELHO => $etapaAtual
+				]
+			);
+		}
+
+		if ($resultado > 0) {
+			http_response_code(200);
+			return array('message' => 'As reuniões foram iniciadas!');
+		} else {
+			http_response_code(500);
+			return array('message' => 'Não foi possível iniciar as reuniões!');
+		}
+	}
+
+	public function finalizarReuniao($dados)
+	{
+		$reunioes = $dados['reunioes'];
+
+		$reuniao = new Reuniao();
+
+		foreach ($reunioes as $r) {
+			$resultado = $reuniao->alterar(
+				[
+					Reuniao::COL_ID => $r,
+					Reuniao::COL_FINALIZADO => 1
+				]
+			);
+		}
+
+		if ($resultado > 0) {
+			http_response_code(200);
+			return array('message'  => 'As reuniões foram finalizadas!');
+		}else{
+			http_response_code(500);
+			return array('message' => 'Não foi possível finalizar as reuniões!');
+		}
+
+	}
+}
