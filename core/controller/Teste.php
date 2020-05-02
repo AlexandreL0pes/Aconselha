25677beefd1d278ea773f2431f61f6a5708dcce3
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-02 11:56:47 -0300

    Remoção de logs para depuração

--- core/controller/Teste.php
+++ core/controller/Teste.php
@@ -2,15 +2,44 @@
 
 namespace core\controller;
 
-class Teste {
+class Teste
+{
 
-    public function teste($dados) {
+    public function teste($dados)
+    {
         // var_dump($dados);
         if (isset($dados['nome']) && $dados['nome'] === '2017103202030090') {
             http_response_code(200);
-            return array ('stauts' => 200, 'message' => 'Deu certo!');
+
+            $alunos = array(
+                [
+                    'firstName' => 'Alexandre',
+                    'lastName' => 'Lopes',
+                    'age' => 20
+                ],
+                [
+                    'firstName' => 'Láyza',
+                    'lastName' => 'Lopes',
+                    'age' => 15
+                ],
+                [
+                    'firstName' => 'Alessandra',
+                    'lastName' => 'Novais',
+                    'age' => 40
+                ]
+            );
+            return json_encode(array('stauts' => 200, 'message' => 'Deu certo!', 'alunos' => $alunos));
         }
         http_response_code(403);
-        return array ('stauts' => 403, 'message' => 'Não deu certo!');
+        return json_encode(array('stauts' => 403, 'message' => 'Não deu certo!'));
+    }
+
+    public function getDados($dados)
+    {
+
+        http_response_code(200);
+        $fakeData = ['Status' => 200, 'Message' => 'Deu certo!'];
+
+        return json_encode($fakeData);
     }
 }
