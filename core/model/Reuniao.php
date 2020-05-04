96b1fe5bc4bed47bfcf82ee7422b7a578d2242c0
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-04 00:40:00 -0300

    Adição da constante de Memória

--- core/model/Reuniao.php
+++ core/model/Reuniao.php
@@ -14,6 +14,7 @@ class Reuniao extends CRUD
     const COL_DATA = 'data';
     const COL_ETAPA_CONSELHO = 'etapaConselho';
     const COL_FINALIZADO = 'finalizado';
+    const COL_MEMORIA = 'memoria';
 
 
     /**
