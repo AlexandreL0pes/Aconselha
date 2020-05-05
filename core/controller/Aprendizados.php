d6574bf13de6811e35acfa059fc9a0b7c515846a
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-05 19:01:42 -0300

    Implementação de métodos do CRUD

--- core/controller/Aprendizados.php
+++ core/controller/Aprendizados.php
@@ -54,4 +54,140 @@ class Aprendizados
             return array('message' => 'Não foi possível cadastrar o aprendizado!');
         }
     }
+
+    public function alterar($dados)
+    {
+        /* {
+            "acao": "Aprendizados/cadastrar",
+          "reuniao": 6,
+            "disciplina": 5,
+            "estudantes": [1,2,3],
+            "descricao": "Uma observação qualquer"
+        } */
+        $aprendizado_id = $dados['aprendizado'];
+        $reuniao = $dados['reuniao'];
+        $disciplina = $dados['disciplina'];
+        $observacao = $dados['descricao'];
+
+        $estudantes = $dados['estudantes'];
+
+        $avaliacao = new Avaliacao();
+
+        $resultadoAvaliacao = $avaliacao->alterar([
+            Avaliacao::COL_ID => $aprendizado_id,
+            Avaliacao::COL_ID_REUNIAO => $reuniao,
+            Avaliacao::COL_PAUTA => $disciplina,
+            Avaliacao::COL_OBSERVACAO => $observacao
+        ]);
+
+        if ($resultadoAvaliacao > 0) {
+            $estudantesAvaliacao = new EstudanteAvaliacao();
+            $estudantesAvaliacao->excluir([EstudanteAvaliacao::COL_ID_AVALIACAO => $aprendizado_id]);
+
+            $erros = array();
+
+            foreach ($estudantes as $estudante) {
+                $resultadoEstudantes = $estudantesAvaliacao->adicionar([
+                    EstudanteAvaliacao::COL_ID_AVALIACAO => $resultadoAvaliacao,
+                    EstudanteAvaliacao::COL_MATRICULA => $estudante
+                ]);
+
+                if (!($resultadoEstudantes > 0)) {
+                    array_push($erros, $estudante);
+                }
+            }
+
+            if (empty($erros)) {
+                http_response_code(200);
+                return array('message' => 'O aprendizado foi alterado com sucesso!');
+            } else {
+                http_response_code(500);
+                return array('message' => 'Houve um erro na alteração dos estudantes!', $erros => $erros);
+            }
+        } else {
+            http_response_code(500);
+            return array('message' => 'Houve um erro na alteração do aprendizado!');
+        }
+    }
+
+    public function selecionar($dados)
+    {
+        $aprendizado_id = $dados['aprendizado'];
+
+        $avaliacao = new Avaliacao();
+
+        $campos = Avaliacao::COL_ID . ", " .
+            Avaliacao::COL_ID_REUNIAO . ", " .
+            Avaliacao::COL_PAUTA . ", " .
+            Avaliacao::COL_DATA . ", " .
+            Avaliacao::COL_OBSERVACAO;
+
+        $busca = [Avaliacao::COL_ID => $aprendizado_id];
+
+        $resultadoAprendizado = $avaliacao->listar($campos, $busca, null, 1)[0];
+        if (!empty($resultadoAprendizado)) {
+            $estudantes = $this->estudantesAprendizado($aprendizado_id);
+
+            // TODO: Selecionar o nome da disciplina pelo id e retornar o nome
+            $disciplina = ['id' => $resultadoAprendizado[Avaliacao::COL_PAUTA], 'nome' => 'Disciplina ' . $resultadoAprendizado[Avaliacao::COL_PAUTA]];
+
+            $aprendizadoCompleto = [
+                'aprendizado' => $resultadoAprendizado[Avaliacao::COL_ID],
+                'reuniao' => $resultadoAprendizado[Avaliacao::COL_ID_REUNIAO],
+                'data' => $resultadoAprendizado[Avaliacao::COL_DATA],
+                'disciplina' => $disciplina,
+                'estudantes' => $estudantes
+            ];
+
+            http_response_code(200);
+            return json_encode($aprendizadoCompleto);
+        } else {
+            http_response_code(500);
+            return array('message' => 'O aprendizado solicitado não foi encontrado!');
+        }
+    }
+
+    private function estudantesAprendizado($aprendizado_id)
+    {
+
+        $estudantesAvaliacao = new EstudanteAvaliacao();
+        $busca = [EstudanteAvaliacao::COL_ID_AVALIACAO => $aprendizado_id];
+
+        $estudantes_id = $estudantesAvaliacao->listar(EstudanteAvaliacao::COL_MATRICULA, $busca, null, 100);
+        $estudantes = [];
+
+        foreach ($estudantes_id as $estudante_id) {
+            $nome = "Estudante " . $estudante_id[EstudanteAvaliacao::COL_MATRICULA];
+            array_push($estudantes, ['id' => $estudante_id[EstudanteAvaliacao::COL_MATRICULA], 'nome' => $nome]);
+        }
+
+        return $estudantes;
+    }
+
+    public function excluir($dados)
+    {
+        $aprendizado_id = $dados['aprendizado'];
+
+        $condicao = [EstudanteAvaliacao::COL_ID_AVALIACAO => $aprendizado_id];
+
+        $estudantesAvaliacao = new EstudanteAvaliacao();
+
+        $retornoEstudantes = $estudantesAvaliacao->excluir($condicao);
+
+        if ($retornoEstudantes) {
+            $aprendizado = new Avaliacao();
+            $condicao = [Avaliacao::COL_ID => $aprendizado_id];
+            $retornoAprendizado = $aprendizado->excluir($condicao);
+            if ($retornoAprendizado) {
+                http_response_code(200);
+                return array('message' => 'O aprendizado foi excluído!');
+            } else {
+                http_response_code(500);
+                return array('message' => 'Houve um erro durante a exclusão dos estudantes');
+            }
+        } else {
+            http_response_code(500);
+            return array('message' => 'Houve um erro na exclusão do aprendizado!');
+        }
+    }
 }
