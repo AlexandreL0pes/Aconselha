7bd44e988d0db2ada990d9d8781021a4cbcfd87a
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-05 14:35:18 -0300

    Renomeação de constante de Experiencia

--- core/model/Experiencia.php
+++ core/model/Experiencia.php
@@ -12,7 +12,7 @@ class Experiencia extends CRUD
     const COL_ID = "id";
     const COL_TITULO = "titulo";
     const COL_OBSERVACAO = "observacao";
-    const COL_REUNIAO = "idReuniao";
+    const COL_ID_REUNIAO = "idReuniao";
     const COL_DATA = "data";
     const COL_CLASSIFICACAO = "idClassificacao";
 
@@ -53,15 +53,15 @@ class Experiencia extends CRUD
     public function listar($campos = null, $busca = [], $ordem = null, $limite = null)
     {
         $campos = $campos != null ? $campos : " * ";
-        $ordem = $ordem != null ? $ordem : " " . self::COL_REUNIAO;
+        $ordem = $ordem != null ? $ordem : " " . self::COL_ID_REUNIAO;
         $limite = $limite != null ? $limite : 100;
 
         $where_condicao = " 1 = 1 ";
         $where_valor = [];
 
-        if (isset($busca[self::COL_REUNIAO]) && !empty($busca[self::COL_REUNIAO])) {
-            $where_condicao .= " AND " . self::COL_REUNIAO . " = ?";
-            $where_valor[] = $busca[self::COL_REUNIAO];
+        if (isset($busca[self::COL_ID_REUNIAO]) && !empty($busca[self::COL_ID_REUNIAO])) {
+            $where_condicao .= " AND " . self::COL_ID_REUNIAO . " = ?";
+            $where_valor[] = $busca[self::COL_ID_REUNIAO];
         }
 
         if (isset($busca[self::COL_CLASSIFICACAO]) && !empty($busca[self::COL_CLASSIFICACAO])) {
