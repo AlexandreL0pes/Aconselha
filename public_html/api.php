51726fbc10fa33e21e0429919189ef8383eaa6ea
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-06 18:10:24 -0300

    Mudança no header da api

--- public_html/api.php
+++ public_html/api.php
@@ -4,7 +4,8 @@
 		require_once '../config.php';
 
 		use core\controller\Api;
-
+		// Lembra de tirar essa merda quando for subir pra produção!!!!
+		header('Access-Control-Allow-Origin: *');
 		$api = new Api();
 		$api->init();
 ?>
