5c9c08142f23ab04e11a804ce95829b65556c307
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-07 18:32:20 -0300

    Preenchimento da Memória de Reunião

--- public_html/assets/js/memoria.js
+++ public_html/assets/js/memoria.js
@@ -1,11 +1,73 @@
-const autoSizeTextarea = () => {
-    const textarea = document.querySelector('#memoriaReuniao');
+const btnSalvarMemoria = document.getElementById("salvar-memoria");
+btnSalvarMemoria.addEventListener("click", (e) => salvarMemoria(e));
 
-    textarea.addEventListener('keydown', (e) => {
-        let element = e.currentTarget;
-        // element.style.height = "5px";
-        element.style.height = (element.scrollHeight) + "px";
+const autoSizeTextarea = () => {
+  const textarea = document.querySelector("#memoriaReuniao");
+
+  textarea.addEventListener("keydown", (e) => {
+    let element = e.currentTarget;
+    // element.style.height = "5px";
+    element.style.height = element.scrollHeight + "px";
+  });
+};
+
+const selecionarMemoria = () => {
+  const reuniao = localStorage.getItem("conselhoAtual") || "";
+  const dados = {
+    acao: "Reunioes/selecionarMemoria",
+    reuniao: reuniao,
+  };
+
+  sendRequest(dados)
+    .then((response) => {
+      preencherMemoria(response.memoria);
+    })
+    .catch((err) => {
+      console.log(err);
     });
 };
 
+const preencherMemoria = (memoria) => {
+  const textareaMemoria = document.querySelector("#memoriaReuniao");
+  textareaMemoria.value = memoria;
+};
+
+const salvarMemoria = (params) => {
+  const reuniao = localStorage.getItem("conselhoAtual") || "";
+  const memoria = document.querySelector("#memoriaReuniao").value;
+  if (memoria !== "" && reuniao !== "") {
+    const dados = {
+      acao: "Reunioes/salvarMemoria",
+      reuniao: reuniao,
+      memoria: memoria,
+    };
+    console.log(dados);
+    sendRequest(dados)
+      .then((response) => {
+        showMessage(
+          "Deu certo!",
+          "A memoria da reunião foi salva com sucesso.",
+          "success",
+          4000
+        );
+      })
+      .catch((err) => {
+        console.error(err);
+        showMessage(
+          "Ops, houve um erro!",
+          "Não foi possível salvar a memória, tente novamente.",
+          "error",
+          5000
+        );
+      });
+  } else {
+    showMessage(
+      "Quase lá!",
+      "Antes de enviar, preencha os campos necessários!",
+      "warning",
+      5000
+    );
+  }
+};
 autoSizeTextarea();
+selecionarMemoria();
