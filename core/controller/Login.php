aa77dba393496b361a2f46070ebf3b7f88870578
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-12 17:20:06 -0300

    Alteração nos retornos da classe Login

--- core/controller/Login.php
+++ core/controller/Login.php
@@ -14,9 +14,10 @@ class Login
         $retorno = Autenticacao::login($dados['login'], $dados['senha'], $dados['lembrar'], false);
         if ($retorno) {
             http_response_code(200);
+            return json_encode(array('message' => 'Deu certo','cookies' => $_COOKIE));
         } else {
             http_response_code(401);
-            return json_encode(array('message' => 'Não foi possível realizar o login!'));
+            return json_encode(array('message' => 'Não foi possível realizar o login!','cookies' => $_COOKIE));
         }
     }
 
