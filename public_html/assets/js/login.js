b3478203be7cbf2ee157fc0965ba9957ae55c056
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-12 17:21:52 -0300

    Adição do arquivo js

---
+++ public_html/assets/js/login.js
@@ -0,0 +1,66 @@
+const listeners = () => {
+  const inputs = document.querySelectorAll(".input");
+  inputs.forEach((input) => {
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
+  });
+
+  const btnLogar = document.getElementById("btnLogar");
+  btnLogar.addEventListener("click", login);
+};
+
+const login = (params) => {
+  const dados = pegarDados();
+
+  if (dados.login != "" && dados.senha != "") {
+    sendRequest(dados)
+      .then((response) => {
+        window.location.href = "./reunioes.html";
+      })
+      .catch((err) => {
+        showMessage(
+          "Houve um erro!",
+          "Não foi possível realizar o login",
+          "error",
+          5000
+				);
+				console.error(err);
+      });
+  } else {
+    showMessage(
+      "Quase lá!",
+      "Alguns campos não foram preenchidos!",
+      "warning",
+      5000
+    );
+  }
+};
+
+const pegarDados = () => {
+  const usuario = document.getElementById("usuario").value || "";
+  const senha = document.getElementById("senha").value || "";
+  const lembrar = true;
+
+  const dados = {
+    acao: "Login/login",
+    login: usuario,
+    senha: senha,
+    lembrar: lembrar,
+  };
+
+  return dados;
+};
+
+function addcl() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
+}
+
+function remcl() {
+  let parent = this.parentNode.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+listeners();
