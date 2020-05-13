8256671c54c056b979a41b72dd35b8c763376ded
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-13 18:17:29 -0300

    Alteração na API

--- public_html/api.php
+++ public_html/api.php
@@ -1,11 +1,28 @@
 <?php
 
-		require_once '../vendor/autoload.php';
-		require_once '../config.php';
+require_once '../vendor/autoload.php';
+require_once '../config.php';
 
-		use core\controller\Api;
-		// Lembra de tirar essa merda quando for subir pra produção!!!!
-		header('Access-Control-Allow-Origin: *');
-		$api = new Api();
-		$api->init();
-?>
+use core\controller\Api;
+// Lembra de tirar essa merda quando for subir pra produção!!!!
+// Antes so tinha isso
+// header('Access-Control-Allow-Origin: *');
+
+// Daqui 
+$origin = $_SERVER['HTTP_ORIGIN'];
+$dominios_permitidos = [
+	'http://localhost:5500',
+	'http://localhost'
+];
+
+if (in_array($origin, $dominios_permitidos)) {
+	header('Access-Control-Allow-Origin: ' . $origin);
+	header('Access-Control-Allow-Credentials: true');
+	header('Access-Control-Allow-Methods: GET, POST');
+	header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
+	header("Access-Control-Allow-Headers: X-Requested-With");
+}
+// Até aqui não tinha 
+$api = new Api();
+$api->init();
