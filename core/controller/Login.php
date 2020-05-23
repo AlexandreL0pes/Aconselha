6b3c13638e30ffe8c692c635e7fd30b36cb00756
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-23 19:01:06 -0300

    Alteração da response de login falho

--- core/controller/Login.php
+++ core/controller/Login.php
@@ -12,13 +12,13 @@ class Login
     public function login($dados)
     {
         $retorno = Autenticacao::login($dados['login'], $dados['senha'], $dados['lembrar'], false);
-        
+
         if ($retorno) {
             http_response_code(200);
             return json_encode(array('message' => 'Logado com sucesso!', 'jwt' => $retorno, 'expireAt' => 5000));
         } else {
             http_response_code(401);
-            return json_encode(array('message' => 'Não foi possível realizar o login!', 'cookies' => $_COOKIE));
+            return json_encode(array('message' => 'Não foi possível realizar o login!'));
         }
     }
 
