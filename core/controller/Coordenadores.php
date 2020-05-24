e3f9648da2ed6ff99cfc49d0147903457d542713
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-24 13:26:37 -0300

    Criação da classe Coordenadores

---
+++ core/controller/Coordenadores.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace core\controller;
+
+use core\model\Usuario;
+use core\sistema\Autenticacao;
+
+class Coordenadores
+{
+
+
+    /*     {
+        "acao": "Coordenadores/cadastrar",
+        "matricula": "123123",
+        "curso": 1,
+        "senha": "criptografia"
+    } */
+    public function cadastrar($dados)
+    {
+        $matricula = $dados['matricula'];
+        $curso = $dados['curso'];
+        $data_inicio = date('Y-m-d');
+        $permissao = Autenticacao::COORDENADOR;
+        $senha = $dados['senha'];
+
+
+        $usuario = new Usuario();
+
+        $retorno = $usuario->adicionar([
+            Usuario::COL_MATRICULA => $matricula,
+            Usuario::COL_CURSO => $curso,
+            Usuario::COL_DATA_INICIO => $data_inicio,
+            Usuario::COL_PERMISSAO => $permissao,
+            Usuario::COL_SENHA => $senha
+        ]);
+
+        if ($retorno > 0) {
+            http_response_code(200);
+            return json_encode(array('message' => "O coordenador foi cadastrado!"));
+        } else {
+            http_response_code(500);
+            return json_encode(array('message' => "Não foi possível cadastrar o coordenador!"));
+        }
+    }
+}
