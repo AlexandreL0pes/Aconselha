3ad8e9c70b7d1a34c6ad8db48b524b0fd2798285
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-27 14:20:55 -0300

    Implementação de coordenador

--- public_html/assets/js/permissoes.js
+++ public_html/assets/js/permissoes.js
@@ -1,12 +1,15 @@
-import {} from "./utils.js";
+import { sendRequest, showMessage } from "./utils.js";
 
-const addCoordenador = document.getElementById("add-coordenador");
-addCoordenador.addEventListener("click", (e) => abrirNovoCoordenador());
+// const addCoordenador = document.getElementById("add-coordenador");
+// addCoordenador.addEventListener("click", (e) => abrirNovoCoordenador());
 
-const abrirNovoCoordenador = (e) => {
-  const modalCoordenador = document.getElementById("modal-coordenador");
-  modalCoordenador.classList.toggle("is-active");
-};
+// const abrirNovoCoordenador = (e) => {
+//   const modalCoordenador = document.getElementById("modal-coordenador");
+//   modalCoordenador.classList.toggle("is-active");
+// };
+
+const btnSalvarCoordenador = document.querySelector(".salvar-coordenador");
+btnSalvarCoordenador.addEventListener("click", (e) => salvarCoordenador(e));
 
 /**
  * Listener para o fechamento do modal
@@ -26,7 +29,7 @@ const closeModal = (params) => {
   });
 };
 
-let autocompleteProfessor = () => {
+let autocompleteCoordenador = () => {
   var api = function (inputValue) {
     return fetch(
       "https://cdn.rawgit.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_titlecase.json"
@@ -68,5 +71,96 @@ let autocompleteProfessor = () => {
   bulmahead("coordenador", "coordenador-menu", api, onSelect, 200);
 };
 
-autocompleteProfessor();
+const salvarCoordenador = (e) => {
+  console.log("> Salvando Coordenador");
+
+  let dados = pegarDados();
+
+  console.log(dados);
+
+  if (dados.acao !== "Coordenadores/alterarSenha" && dados.matricula == null) {
+    showMessage(
+      "Confira seus dados!",
+      "É necessário informar o novo coordenador.",
+      "warning",
+      4000
+    );
+    return false;
+  }
+
+  if (dados.curso != "") {
+    sendRequest(dados)
+      .then((response) => {
+        console.log(response);
+        showMessage(
+          "Deu certo!",
+          "O coordenador foi alterado com sucesso!",
+          "success",
+          4000
+        );
+      })
+      .catch((err) => {
+        console.error(err);
+        showMessage(
+          "Houve um erro",
+          "Não foi possível salvar o coordenador, verifique os dados.",
+          "warning",
+          400
+        );
+      });
+  } else {
+    showMessage(
+      "Confira seus dados!",
+      "Existe algum erro no formulário!",
+      "warning",
+      4000
+    );
+  }
+};
+
+const pegarDados = () => {
+  const coordenadorNovo = document
+    .getElementById("coordenador")
+    .getAttribute("data-coordenador");
+  const senha = document.getElementById("senha-coordenador").value;
+  const curso = document
+    .getElementById("modal-coordenador")
+    .getAttribute("data-curso");
+
+  const coordenadorAtual =
+    document
+      .getElementById("modal-coordenador")
+      .getAttribute("data-coordenador") || "";
+
+  let dados = {
+    curso: curso,
+    senha: senha,
+  };
+
+  // Caso de trocar a senha
+  dados.acao = "Coordenadores/alterarSenha";
+
+  // Caso não exista coordenador atual
+  if (coordenadorAtual === "") {
+    dados = {
+      curso: curso,
+      matricula: coordenadorNovo,
+      senha: senha,
+    };
+    dados.acao = "Coordenadores/adicionar";
+  }
+
+
+  // Caso o novo id seja diferente do antigo
+  if (coordenadorNovo !== coordenadorAtual && coordenadorAtual !== "") {
+    dados = {
+      curso: curso,
+      matricula: coordenadorNovo,
+      senha: senha,
+    };
+    dados.acao = "Coordenadores/atualizarCoordenador";
+  }
+  return dados;
+};
+autocompleteCoordenador();
 closeModal();
