63b18dab3a29630bc5c865d20c36743c3d1b1180
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-27 10:27:52 -0300

    Implementação do autoCompleteProfessor

--- public_html/assets/js/permissoes.js
+++ public_html/assets/js/permissoes.js
@@ -1,5 +1,72 @@
-import {  } from "./utils.js";
+import {} from "./utils.js";
 
+const addCoordenador = document.getElementById("add-coordenador");
+addCoordenador.addEventListener("click", (e) => abrirNovoCoordenador());
 
+const abrirNovoCoordenador = (e) => {
+  const modalCoordenador = document.getElementById("modal-coordenador");
+  modalCoordenador.classList.toggle("is-active");
+};
 
+/**
+ * Listener para o fechamento do modal
+ * @param {*} params
+ */
+const closeModal = (params) => {
+  const modals = document.querySelectorAll(".modal");
+  modals.forEach((modal) => {
+    const closeBtn = modal.querySelector(".modal-close-btn");
+    closeBtn.addEventListener("click", (evnt) => {
+      modal.classList.toggle("is-active");
+    });
+    const bgModal = modal.querySelector(".modal-background");
+    bgModal.addEventListener("click", (evnt) => {
+      modal.classList.toggle("is-active");
+    });
+  });
+};
 
+let autocompleteProfessor = () => {
+  var api = function (inputValue) {
+    return fetch(
+      "https://cdn.rawgit.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_titlecase.json"
+    )
+      .then(function (resp) {
+        return [
+          { label: "Adriano Braga", value: "1459180" },
+          { label: "Lucas Faria", value: "97312" },
+          { label: "Rangel Rigo", value: "417530" },
+          { label: "Marcos Morais", value: "914402" },
+          { label: "Jaqueline Ribeiro", value: "81183" },
+          { label: "Ramayane Braga", value: "41917" },
+        ];
+        // return resp.json();
+      })
+      .then(function (states) {
+        return states.filter(function (state) {
+          return state.label.startsWith(inputValue);
+        });
+      })
+      .then(function (filtered) {
+        return filtered.map(function (state) {
+          return { label: state.label, value: state.value };
+        });
+      })
+      .then(function (transformed) {
+        return transformed.slice(0, 5);
+      });
+  };
+
+  var onSelect = function (state) {
+    console.log("> O brabo tem nome");
+    console.log(state);
+
+    const input = document.querySelector("#coordenador");
+    input.setAttribute("data-coordenador", state.value);
+  };
+
+  bulmahead("coordenador", "coordenador-menu", api, onSelect, 200);
+};
+
+autocompleteProfessor();
+closeModal();
