06e2c734a71e1e9feec8cb814a6d895843421761
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-28 18:24:35 -0300

    Implementação da mode\Aluno

---
+++ core/model/Aluno.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace core\model;
+
+use core\CRUD;
+
+class Aluno extends CRUD
+{
+
+
+    const TABELA = "ALUNOS";
+    const COL_MATRICULA = "MATRICULA";
+    const COL_NOME_PESSOA = "NOME_PESSOA";
+    const COL_COD_TURMA_ATUAL = "COD_TURMA_ATUAL";
+
+    public function listar($campos = null, $busca = [], $ordem = null, $limite = null)
+    {
+        $database = "academico";
+
+        $campos = $campos != null ? $campos : " * ";
+        $ordem = $ordem != null ? $ordem : " ";
+        $limite = $limite != null ? " TOP {$limite} " : " TOP 1000 ";
+
+        $campos = $limite . " " . $campos;
+
+
+        $tabela = self::TABELA;
+        $where_condicao = " 1 = 1 ";
+        $where_valor = [];
+
+
+        if ($busca && count($busca) > 0) {
+            if ($busca[self::COL_COD_TURMA_ATUAL] && !empty($busca[self::COL_COD_TURMA_ATUAL])) {
+                $where_condicao .= " AND " . self::COL_COD_TURMA_ATUAL . " = ? ";
+                $where_valor[] = $busca[self::COL_COD_TURMA_ATUAL];
+
+                $tabela = self::TABELA .
+                    " INNER JOIN MATRICULAS ON ALUNOS.COD_ALUNO = MATRICULAS.COD_ALUNO " .
+                    " INNER JOIN PESSOAS ON ALUNOS.COD_PESSOA = PESSOAS.COD_PESSOA ";
+
+                $ordem = "PESSOAS.NOME_PESSOA";
+            }
+        }
+
+        $retorno = [];
+
+        try {
+            $retorno = $this->read($database, $tabela, $campos, $where_condicao, $where_valor, null, $ordem, null);
+            // echo $this->pegarUltimoSQL();
+        } catch (\Throwable $th) {
+            echo $this->pegarUltimoSQL();
+
+            echo "Mensagem: " . $th->getMessage() . "\n Local: " . $th->getTraceAsString();
+
+            return false;
+        }
+
+        return $retorno;
+    }
+}
