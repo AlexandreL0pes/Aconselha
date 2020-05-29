8647ca02d974984c7010766c3dce7d343669280c
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-05-29 18:21:50 -0300

    Implementação do controller\Alunos

---
+++ core/controller/Alunos.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace core\controller;
+
+
+use core\model\Aluno;
+
+
+
+class Alunos {
+
+
+    public function selecionar($dados = [])
+    {
+        $matriculaAluno = $dados['matricula'];
+
+
+        $campos = Aluno::COL_MATRICULA . ", " . 
+        "CONCAT(SUBSTRING(PESSOAS.NOME_PESSOA, 1, CHARINDEX(' ', PESSOAS.NOME_PESSOA) - 1),' ', REVERSE(SUBSTRING(REVERSE(PESSOAS.NOME_PESSOA), 1, CHARINDEX(' ', REVERSE(PESSOAS.NOME_PESSOA)) - 1))) as nome";
+
+        $busca = [Aluno::COL_MATRICULA => $matriculaAluno];
+
+        $aluno = new Aluno();
+
+        $retornoAluno = ($aluno->listar($campos, $busca, null, 1))[0];
+
+        // print_r($retornoAluno);
+        if (!empty($retornoAluno)) {
+            
+            $alunoJSON = [
+                'nome' => $retornoAluno['nome'],
+                'matricula' => $retornoAluno[Aluno::COL_MATRICULA]
+            ];
+
+            http_response_code(200);
+            return json_encode($alunoJSON);
+
+        }else{
+            http_response_code(500);
+            return json_encode(array('message' => 'Nenhum aluno foi encontrado!'));
+        }
+    }
+}
