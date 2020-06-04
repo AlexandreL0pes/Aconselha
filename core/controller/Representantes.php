90088230be4da755d51dd0310df825af22e9e86d
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-06-04 17:17:13 -0300

    Implementação de cadastro de representantes

--- core/controller/Representantes.php
+++ core/controller/Representantes.php
@@ -12,19 +12,40 @@ class Representantes
 
     public function cadastrar($dados)
     {
-    
-        
+
+
         // COD_MATRICULA 
         // COD_TURMA 
         // senha
 
 
         // Ver se a matrícula está em um representante atual, se tiver altera, senão cadastra
-        $matricula = $dados['curso'];
+        $matricula = $dados['matricula'];
+        $turma = $dados['turma'];
+        $senha = $dados['senha'];
+
         $data_inicio = date('Y-m-d');
+
         $permissao = Autenticacao::REPRESENTANTE;
-        
+
         $usuario = new Usuario();
+
+
+        $retorno = $usuario->adicionar([
+            Usuario::COL_MATRICULA => $matricula,
+            Usuario::COL_TURMA => $turma,
+            Usuario::COL_SENHA => $senha,
+            Usuario::COL_PERMISSAO => $permissao,
+            Usuario::COL_DATA_INICIO => $data_inicio
+        ]);
+
+        if ($retorno > 0) {
+            http_response_code(200);
+            return json_encode(array('message' => 'O representante foi cadastrado!'));
+        } else {
+            http_response_code(500);
+            return json_encode(array('message' => 'Não foi possível cadastrar o representante!'));
+        }
     }
 
     public function representanteCadastrado($matricula)
