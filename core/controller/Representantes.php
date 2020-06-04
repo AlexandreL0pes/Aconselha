e0b3f701bb7b82117057af3fe31e537080235793
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-06-04 16:23:37 -0300

    Implementação de verificação de representante

--- core/controller/Representantes.php
+++ core/controller/Representantes.php
@@ -10,6 +10,38 @@ use core\sistema\Autenticacao;
 class Representantes
 {
 
+    public function cadastrar($dados)
+    {
+    
+        
+        // COD_MATRICULA 
+        // COD_TURMA 
+        // senha
+
+
+        // Ver se a matrícula está em um representante atual, se tiver altera, senão cadastra
+        $matricula = $dados['curso'];
+        $data_inicio = date('Y-m-d');
+        $permissao = Autenticacao::REPRESENTANTE;
+        
+        $usuario = new Usuario();
+    }
+
+    public function representanteCadastrado($matricula)
+    {
+        $usuario = new Usuario();
+
+        $campos = Usuario::COL_ID . ", " . Usuario::COL_MATRICULA;
+        $busca = [
+            Usuario::COL_MATRICULA => $matricula,
+            'periodo' => 'atual',
+            Usuario::COL_PERMISSAO => Autenticacao::REPRESENTANTE
+        ];
+
+        $representante = ($usuario->listar($campos, $busca, null, 1))[0];
+
+        return !empty($representante);
+    }
 
     public function selecionarRepresentantesAtuais($turma)
     {
