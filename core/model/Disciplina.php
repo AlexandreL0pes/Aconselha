a4d6ef79cb892a511e694b8d86efb11e74bf5509
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-06-06 20:04:51 -0300

    add: Implementação da model\Disciplina

---
+++ core/model/Disciplina.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace core\model;
+
+use core\CRUD;
+
+
+class Disciplina extends CRUD
+{
+
+    const TABELA = "DISCIPLINAS";
+    const COL_COD_DISCIPLINA = "COD_DISCIPLINA";
+    const COL_DESC_DISCIPLINA = "DESC_DISCIPLINA";
+    const COL_CURSO = "COD_CURSO";
+    const COL_TURMA = "COD_TURMA";
+
+    public function listar($campos = null, $busca = [], $ordem = null, $limite = null)
+    {
+        $database = "academico";
+
+        $campos = $campos != null ? $campos : " * ";
+        $ordem = $ordem != null ? $ordem : self::COL_COD_DISCIPLINA;
+        $limite = $limite != null ? " TOP {$limite} " : " TOP 1000 ";
+
+        $campos = $limite . " " . $campos;
+
+        $tabela = self::TABELA;
+
+        $where_condicao = " 1 = 1 ";
+        $where_valor = [];
+
+
+        if ($busca && count($busca) > 0) {
+            if (isset($busca[self::COL_CURSO]) && !empty($busca[self::COL_CURSO])) {
+                $where_condicao .= " AND " . self::COL_CURSO . " = ? ";
+                $where_valor[] = $busca[self::COL_CURSO];
+
+                $tabela = self::TABELA .
+                    " INNER JOIN DISCIPLINAS_MATRIZES_CURRICULARES on DISCIPLINAS.COD_DISCIPLINA = DISCIPLINAS_MATRIZES_CURRICULARES.COD_DISCIPLINA " .
+                    " INNER JOIN TURMAS on DISCIPLINAS_MATRIZES_CURRICULARES.COD_MATRIZ_CURRICULAR = TURMAS.COD_MATRIZ_CURRICULAR ";
+            }
+
+            if (isset($busca[self::COL_TURMA]) && !empty($busca[self::COL_TURMA])) {
+                $where_condicao .= " AND " . self::COL_TURMA . " = ? ";
+                $where_valor[] = $busca[self::COL_TURMA];
+
+                $tabela = self::TABELA .
+                    " INNER JOIN DISCIPLINAS_MATRIZES_CURRICULARES on DISCIPLINAS.COD_DISCIPLINA = DISCIPLINAS_MATRIZES_CURRICULARES.COD_DISCIPLINA " .
+                    " INNER JOIN TURMAS on DISCIPLINAS_MATRIZES_CURRICULARES.COD_MATRIZ_CURRICULAR = TURMAS.COD_MATRIZ_CURRICULAR ";
+            }
+        }
+
+        $retorno = [];
+
+        try {
+            $retorno = $this->read($database, $tabela, $campos, $where_condicao, $where_valor, null, $ordem, null);
+        } catch (\Throwable $th) {
+            echo "Mensagem: " . $th->getMessage() . "\n Local: " . $th->getTraceAsString();
+            return false;
+        }
+
+        return $retorno;
+    }
+}
