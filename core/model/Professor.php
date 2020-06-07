9ec1105eb75cfcc75f272395736e1fa4dc5bb9b4
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-06-07 14:52:47 -0300

    add: Implementação de model\Professor

---
+++ core/model/Professor.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace core\model;
+
+use core\CRUD;
+
+
+class Professor extends CRUD
+{
+
+
+    const TABELA = "PROFESSORES";
+    const COL_COD_PESSOA = "COD_PESSOA";
+    const COL_COD_INSTITUICAO = "COD_INSTITUICAO";
+    
+    public function listar($campos = null, $busca = [], $ordem = null, $limite = null)
+    {
+        $database = "academico";
+
+        $campos = $campos != null ? $campos : " * ";
+        $ordem = $ordem != null ? $ordem : self::TABELA . "." . self::COL_COD_PESSOA;
+        $limite = $limite != null ? " TOP {$limite} " : " TOP 1000 ";
+
+
+        $campos = $limite . " " . $campos;
+
+        $tabela = self::TABELA;
+        $where_condicao = " 1 = 1 ";
+        $where_valor = [];
+
+        if ($busca && count($busca) > 0) {
+            if (isset($busca[self::COL_COD_PESSOA]) && !empty($busca[self::COL_COD_PESSOA])) {
+                $where_condicao .= " AND " . self::TABELA . "." . self::COL_COD_PESSOA . " = ? ";
+                $where_valor[] = $busca[self::COL_COD_PESSOA];
+
+                $tabela = self::TABELA .
+                    " INNER JOIN PAUTAS ON PROFESSORES.COD_PROFESSOR = PAUTAS.COD_PROFESSOR " .
+                    " INNER JOIN PESSOAS ON PROFESSORES.COD_PESSOA = PESSOAS.COD_PESSOA ";
+            }
+
+            if (isset($busca['ano_letivo']) && !empty($busca['ano_letivo'])) {
+
+                if ($busca['ano_letivo'] == 'atual') {
+                    $where_condicao .= " AND ANO_LET = YEAR(GETDATE()) ";
+                } else {
+                    $where_condicao .= " AND ANO_LET = ? ";
+                    $where_valor[] = $busca['ano_letivo'];
+                }
+            }
+        }
+
+        // Filtrando apenas os professores do CAMPUS CERES
+        $where_condicao .= " AND " . self::TABELA . "." . self::COL_COD_INSTITUICAO . " = ? ";
+        $where_valor[] = '3';
+
+        // Filtratndo apenas as pautas dos cursos técnicos integrados
+        $where_condicao .= " AND COD_TIPO_CURSO = ?";
+        $where_valor[] = '265';
+
+        $retorno = [];
+
+        try {
+            $retorno = $this->read($database, $tabela, $campos, $where_condicao, $where_valor, null, null);
+            // echo $this->pegarUltimoSQL();
+        } catch (\Throwable $th) {
+            echo $this->pegarUltimoSQL();
+            echo "Mensagem: " . $th->getMessage() . "\n Local: " . $th->getTraceAsString();
+            return false;
+        }
+
+        return $retorno;
+    }
+}
