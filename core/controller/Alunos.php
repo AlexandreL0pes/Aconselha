6a5e4f04c0c546681d86bb51c3e2a72bcf358e0a
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-06-08 10:13:33 -0300

    Alteração na assinatura de método

--- core/controller/Alunos.php
+++ core/controller/Alunos.php
@@ -11,34 +11,29 @@ use core\model\Aluno;
 class Alunos {
 
 
-    public function selecionar($dados = [])
+    public function selecionar($matricula)
     {
-        $matriculaAluno = $dados['matricula'];
+        # $matriculaAluno = $dados['matricula'];
 
 
         $campos = Aluno::COL_MATRICULA . ", " . 
         // "CONCAT(SUBSTRING(PESSOAS.NOME_PESSOA, 1, CHARINDEX(' ', PESSOAS.NOME_PESSOA) - 1),' ', REVERSE(SUBSTRING(REVERSE(PESSOAS.NOME_PESSOA), 1, CHARINDEX(' ', REVERSE(PESSOAS.NOME_PESSOA)) - 1))) as nome";
         "{fn CONCAT(SUBSTRING(PESSOAS.NOME_PESSOA, 1, CHARINDEX(' ', PESSOAS.NOME_PESSOA) - 1), {fn CONCAT(' ', REVERSE(SUBSTRING(REVERSE(PESSOAS.NOME_PESSOA), 1, CHARINDEX(' ', REVERSE(PESSOAS.NOME_PESSOA)) - 1)))})} as nome";
 
-        $busca = [Aluno::COL_MATRICULA => $matriculaAluno];
+        $busca = [Aluno::COL_MATRICULA => $matricula];
 
-        $aluno = new Aluno();
+        $a = new Aluno();
 
-        $retornoAluno = ($aluno->listar($campos, $busca, null, 1))[0];
+        $aluno = ($a->listar($campos, $busca, null, 1))[0];
 
-        if (!empty($retornoAluno)) {
+        if (!empty($aluno)) {
             
-            $alunoJSON = [
-                'nome' => $retornoAluno['nome'],
-                'matricula' => $retornoAluno[Aluno::COL_MATRICULA]
+            $aluno = [
+                'nome' => $aluno['nome'],
+                'matricula' => $aluno[Aluno::COL_MATRICULA]
             ];
-
-            http_response_code(200);
-            return json_encode($alunoJSON);
-
-        }else{
-            http_response_code(500);
-            return json_encode(array('message' => 'Nenhum aluno foi encontrado!'));
         }
+
+        return $aluno;
     }
 }
