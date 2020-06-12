7da71c676ce5e3ed698dfcb7e5429cd1307cda74
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-06-12 19:16:47 -0300

    add: Método selecionarConselheiro

--- core/controller/Conselheiros.php
+++ core/controller/Conselheiros.php
@@ -4,6 +4,7 @@
 
 namespace core\controller;
 
+use core\model\Turma;
 use core\model\Usuario;
 use core\sistema\Autenticacao;
 
@@ -47,5 +48,29 @@ class Conselheiros
 
         $conselheiro = $usuario->listar($campos, $busca, null, 1)[0];
         return $conselheiro;
+    }
+
+    public function selecionarConselheiro($dados)
+    {
+        $turma_id = $dados['turma'];
+        $t = new Turmas();
+        $turma = $t->informacoesTurma(['turma' => $turma_id]);
+        $turma = json_decode($turma, true);
+        $conselheiro = $this->obterConselheiro($turma_id);
+        $conselheiro = json_decode($conselheiro, true);
+
+        $retorno = [
+            'turma' => $turma,
+            'conselheiro' => $conselheiro
+        ];
+
+        if ($turma > 0 && $conselheiro > 0) {
+
+            http_response_code(200);
+            return json_encode($retorno);
+        } else {
+            http_response_code(500);
+            return json_encode(array("message" => "Não foi possível selecionar o conselheiro"));
+        }
     }
 }
