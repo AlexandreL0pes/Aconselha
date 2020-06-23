e76ebc3e8ed9593dd5814f2f5e86f428b9650fd5
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-06-23 10:06:45 -0300

    Implementação de listagem de medias por matrícula

--- core/controller/MedidasDisciplinares.php
+++ core/controller/MedidasDisciplinares.php
@@ -7,12 +7,12 @@ use core\model\MedidaDisciplinar;
 
 class MedidasDisciplinares
 {
-    
+
     /**
      * Retorna todas as medidas disciplinares dos estudantes de uma turma
      *
      * @param  mixed $cod_turma
-     * @return void
+     * @return array
      */
     public function listarMedidasTurma($cod_turma = null)
     {
@@ -29,6 +29,32 @@ class MedidasDisciplinares
         $medidas = $md->listar($campos, $busca, null, null);
 
         $retorno = [];
+
+        if (!empty($medidas)) {
+            $retorno = $medidas;
+        }
+
+        return $retorno;
+    }
+
+    /**
+     * Retorna todas as medidas disciplinares de um estudante
+     *
+     * @param  mixed $matricula
+     * @return array
+     */
+    public function listarMedidasMatricula($matricula = null)
+    {
+
+        $campos = MedidaDisciplinar::COL_COD_MEDIDA_DISCIPLINAR . ", " .
+            MedidaDisciplinar::COL_MATRICULA . ", " .
+            MedidaDisciplinar::COL_DATA . ", " .
+            MedidaDisciplinar::COL_DESC_TIPO_MEDIDA_DISCIPLINAR;
+
+        $busca = [MedidaDisciplinar::COL_MATRICULA => $matricula];
+
+        $md = new MedidaDisciplinar();
+        $medidas = $md->listar($campos, $busca, MedidaDisciplinar::COL_DATA . " DESC ", null);
 
         if (!empty($medidas)) {
             $retorno = $medidas;
