610bf9220a70161fede6d873e9399bcd48d18465
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-06-26 11:58:11 -0300

    Implementação do gráfico de coeficiente geral

--- public_html/assets/js/dashboard_turma.js
+++ public_html/assets/js/dashboard_turma.js
@@ -14,6 +14,51 @@ const listener = () => {
   btnRemoverFiltro.addEventListener("click", removerFiltro);
 };
 
+const gerarGraficoCoef = (qtdAlto, qtdMedio, qtdBaixo) => {
+    var ctx = document.getElementById("coef-geral").getContext("2d");
+    var myChart = new Chart(ctx, {
+      type: "doughnut",
+      responsive: true,
+      data: {
+        datasets: [
+          {
+            data: [qtdAlto, qtdMedio, qtdBaixo],
+            backgroundColor: ["#28ac47", "#3d21f1", "#f22f2f"],
+          },
+        ],
+        labels: ["Alto", "Médio", "Baixo"],
+      },
+  
+      options: {
+        legend: {
+          display: false,
+        },
+        cutoutPercentage: 70,
+      },
+    });
+  };
+  
+  const obterCoef = () => {
+    const turma = localStorage.getItem("turmaAtual");
+  
+    if (turma !== null) {
+      const dados = {
+        acao: "Turmas/obterQuantidadeConficienteGeral",
+        turma: turma,
+      };
+  
+      sendRequest(dados)
+        .then((response) => {
+          console.log(response);
+          gerarGraficoCoef(response.alto, response.medio, response.baixo);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  };
+  
+
 const obterEstatisticaTurma = () => {
   const turma = localStorage.getItem("turmaAtual");
   if (turma !== null) {
@@ -90,3 +135,4 @@ const gerarChip = (perfil) => {
 
 obterEstatisticaTurma();
 obterPrincipaisAvaliacoes();
+obterCoef();
