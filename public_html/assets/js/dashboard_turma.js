5dc964664cb36df7cf276f8ca1b25d63e4bcff34
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-06-26 11:52:29 -0300

    Listagem de principais avaliações diagnósticas

--- public_html/assets/js/dashboard_turma.js
+++ public_html/assets/js/dashboard_turma.js
@@ -33,16 +33,60 @@ const obterEstatisticaTurma = () => {
 };
 
 const apresentarEstatisticasTurma = (estatistica) => {
-    const estatisticaTurma = document.getElementById("estatistica-turma");
-  
-    estatisticaTurma.querySelector(".coef-geral .resultado").innerHTML =
-      estatistica.coeficiente_geral;
-    estatisticaTurma.querySelector(".experiencia .resultado").innerHTML =
-      estatistica.aprendizados;
-    estatisticaTurma.querySelector(".aprendizado .resultado").innerHTML =
-      estatistica.experiencias;
-    estatisticaTurma.querySelector(".medidas .resultado").innerHTML =
-      estatistica.medidas_disciplinares;
-  };
+  const estatisticaTurma = document.getElementById("estatistica-turma");
 
-  obterEstatisticaTurma()
+  estatisticaTurma.querySelector(".coef-geral .resultado").innerHTML =
+    estatistica.coeficiente_geral;
+  estatisticaTurma.querySelector(".experiencia .resultado").innerHTML =
+    estatistica.aprendizados;
+  estatisticaTurma.querySelector(".aprendizado .resultado").innerHTML =
+    estatistica.experiencias;
+  estatisticaTurma.querySelector(".medidas .resultado").innerHTML =
+    estatistica.medidas_disciplinares;
+};
+
+const obterPrincipaisAvaliacoes = () => {
+  const turma = localStorage.getItem("turmaAtual");
+  if (turma !== null) {
+    const dados = { acao: "Perfis/listarPerfisRelevantes", turma: turma };
+
+    sendRequest(dados)
+      .then((response) => {
+        apresentarPrincipaisAvaliacoes(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+};
+
+const apresentarPrincipaisAvaliacoes = (dados) => {
+  console.log(dados[0]);
+  if (dados[0].nome !== undefined) {
+    const avaliacoes = document.getElementById("avaliacoes");
+    dados.map((perfil) => {
+      avaliacoes.appendChild(gerarChip(perfil));
+    });
+  } else {
+    const avaliacoes = document.getElementById("avaliacoes");
+    avaliacoes.innerHTML = "Nenhuma avaliação foi encontrada";
+  }
+};
+
+const gerarChip = (perfil) => {
+  const chip = document.createElement("span");
+  chip.classList.add("chip");
+
+  if (perfil.tipo === "1") {
+    chip.classList.add("positivo");
+  } else {
+    chip.classList.add("negativo");
+  }
+
+  chip.innerText = perfil.nome;
+
+  return chip;
+};
+
+obterEstatisticaTurma();
+obterPrincipaisAvaliacoes();
