6743b0328d19b6c48cc99b17a16bcff49f21e7dd
AlexandreL0pes <alexandre9999lopes@gmail.com>
2020-06-27 21:23:45 -0300

    Adição de permissões em coordenadores

--- public_html/coordenador/memoria.php
+++ public_html/coordenador/memoria.php
@@ -38,7 +38,7 @@ if (!Autenticacao::isCoordenador($_COOKIE['token'])) {
 
     <!-- Header -->
     <header class="header">
-      <div class="dropdown is-right is-small no-border" id="dropdown-user">
+    <div class="dropdown is-right is-small no-border" id="dropdown-user">
         <div class="dropdown-trigger">
           <button class="button" aria-haspopup="true" aria-controls="dropdown-menu6">
             <span>Coordenador</span>
@@ -50,16 +50,12 @@ if (!Autenticacao::isCoordenador($_COOKIE['token'])) {
         <div class="dropdown-menu" id="dropdown-menu6" role="menu">
           <div class="dropdown-content">
             <div class="dropdown-item">
-              <a href="#" class="dropdown-item">
+              <a href="../professor/turmas.php" class="dropdown-item">
                 Professor
               </a>
               <hr class="dropdown-divider" />
-              <a href="#" class="dropdown-item">
+              <a href="../gerencia/reunioes.php" class="dropdown-item">
                 Coordenador
-              </a>
-              <hr class="dropdown-divider" />
-              <a href="#" class="dropdown-item">
-                Aluno
               </a>
             </div>
           </div>
